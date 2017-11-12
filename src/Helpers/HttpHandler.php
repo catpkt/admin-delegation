8de478f08a20efacd2dcea6614b7c51a4391312e
Fenz <uukooo@126.com>
2017-11-12 23:48:51 +0800

    Helpers\HttpHandler. 添加各种请求方法的处理逻辑，分别有 获取元数据、列出资源、获取资源、创建资源、修改资源、删除资源

--- src/Helpers/HttpHandler.php
+++ src/Helpers/HttpHandler.php
@@ -85,4 +85,146 @@ class HttpHandler
 		);
 	}
 
+	/**
+	 * Method method_META
+	 *
+	 * @access protected
+	 *
+	 * @param  array $headers
+	 * @param  Request $request
+	 *
+	 * @return Response
+	 */
+	protected function method_META( array$headers, Request$request ):Response
+	{
+		return new Response( ($this->encryptor)( $this->meta ) );
+	}
+
+	/**
+	 * Method method_LIST
+	 *
+	 * @access protected
+	 *
+	 * @param  array $headers
+	 * @param  Request $request
+	 *
+	 * @return Response
+	 */
+	protected function method_LIST( array$headers, Request$request ):Response
+	{
+		list( $controller, $resource, )= $this->meta->loadController( $headers['Resource-Path'] );
+
+		$list= $controller->list( $resource, $headers['Take']??16, $headers['Skip']??0, $headers['Filters']??[] );
+
+		foreach( $list as $key=>$resource )
+		{
+			if(!( $resource instanceof __\IResource ))
+			{
+				throw new \TypeError( 'Return value of '.get_class( $controller ).'->list() must be an array of instances of '.__\IResource::class.', strange things returned' );
+			}
+
+			$list[$key]= [ 'id'=>$resource->getId(), 'data'=>$resource->getData(), ];
+		}
+
+		return new Response( ($this->encryptor)( $list ) );
+	}
+
+	/**
+	 * Method method_GET
+	 *
+	 * @access protected
+	 *
+	 * @param  array $headers
+	 * @param  Request $request
+	 *
+	 * @return Response
+	 */
+	protected function method_GET( array$headers, Request$request ):Response
+	{
+		list( $controller, $resource, )= $this->meta->loadController( $headers['Resource-Path'] );
+
+		$resource= $controller->get( $resource, $headers['Response-Id'] );
+
+		return new Response( ($this->encryptor)( [ 'id'=>$resource->getId(), 'data'=>$resource->getData(), ] ) );
+	}
+
+	/**
+	 * Method method_POST
+	 *
+	 * @access protected
+	 *
+	 * @param  array $headers
+	 * @param  Request $request
+	 *
+	 * @return Response
+	 */
+	protected function method_POST( array$headers, Request$request ):Response
+	{
+		list( $controller, $resource, )= $this->meta->loadController( $headers['Resource-Path'] );
+
+		$result= $controller->create( $resource, $this->encryptor->decrypt( $request->getContent() ) );
+
+		return new Response( ($this->encryptor)( $result ) );
+	}
+
+	/**
+	 * Method method_PATCH
+	 *
+	 * @access protected
+	 *
+	 * @param  array $headers
+	 * @param  Request $request
+	 *
+	 * @return Response
+	 */
+	protected function method_PATCH( array$headers, Request$request ):Response
+	{
+		list( $controller, $resource, )= $this->meta->loadController( $headers['Resource-Path'] );
+
+		$result= $controller->update( $resource, $headers['Response-Id'], $this->encryptor->decrypt( $request->getContent() ) );
+
+		return new Response( ($this->encryptor)( $result ) );
+	}
+
+	/**
+	 * Method method_DELETE
+	 *
+	 * @access protected
+	 *
+	 * @param  array $headers
+	 * @param  Request $request
+	 *
+	 * @return Response
+	 */
+	protected function method_DELETE( array$headers, Request$request ):Response
+	{
+		list( $controller, $resource, )= $this->meta->loadController( $headers['Resource-Path'] );
+
+		$result= $controller->delete( $resource, ...$headers['Response-Ids'] );
+
+		return new Response( ($this->encryptor)( $result ) );
+	}
+
+	/**
+	 * Method method_OPTIONS
+	 *
+	 * @access protected
+	 *
+	 * @param  array $headers
+	 * @param  Request $request
+	 *
+	 * @return Response
+	 */
+	protected function method_OPTIONS( array$headers, Request$request ):Response
+	{
+		$response= new Response();
+
+		$methods= 'GET, POST, PATCH, DELETE, OPTIONS';
+
+		$response->headers->set( 'Allow', $methods );
+		$response->headers->set( 'Access-Control-Allow-Methods', $methods );
+
+		return $response;
+	}
+
 }
