424c17fa45a4849a11759a6ea347483a6abd7ddf
Fenz <uukooo@126.com>
2018-08-24 19:42:37 +0800

    增加地理坐标字段类型

---
+++ src/Fields/LocationField.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CatPKT\AdminDelegation\Fields;
+
+use CatPKT\AdminDelegation as __;
+
+////////////////////////////////////////////////////////////////
+
+/**
+ * 地理坐标 字段
+ *
+ * @method  showInList()   在列表中显示字段
+ */
+class LocationField extends AField
+{
+	
+}
